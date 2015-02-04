89af390bfb866151aa4df002cfd8493157416fff
Daniel Moffat <danmofo@gmail.com>
2015-02-04 09:01:29 +0000

    created guitars section view routes

--- src/project/guitars/views.py
+++ src/project/guitars/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-from project import db
-from project.views import login_required
+from project        import db
+from project.views  import login_required
 from project.models import Guitar
 
 guitars_blueprint = Blueprint(
@@ -11,11 +11,35 @@ guitars_blueprint = Blueprint(
     template_folder='../templates/guitars',
     static_folder='../assets'
 )
+
 @guitars_blueprint.route('/browse')
 def browse():
     guitars = Guitar.query.all()
     return render_template('browse_guitars.jinja.html', guitars=guitars)
 
-@guitars_blueprint.route('/browse/<int:guitar_id>')
-def browse_specific(guitar_id):
-    pass
+# todo: figure out how to make these URLs case insensitive
+
+# e.g. /guitars/Gibson
+@guitars_blueprint.route('/<brand>')
+def browse_by_brand(brand):
+    guitars = Guitar.query.filter_by(brand=brand).all()
+    print guitars
+    return ''
+
+# e.g. /guitars/Gibson/SG
+@guitars_blueprint.route('/<brand>/<model>')
+def browse_by_model(brand, model):
+    guitars = Guitar.query.filter_by(brand=brand, model=model).all()
+    print guitars
+    return ''
+
+# e.g. /guitars/Gibson/SG/2015
+@guitars_blueprint.route('/<brand>/<model>/<int:year>')
+def browse_by_year(brand, model, year):
+    guitars = Guitar.query.filter_by(
+        brand=brand,
+        model=model,
+        year = year
+    ).all()
+    print guitars
+    return ''
